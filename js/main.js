0b2bb171cce650c87f0de33327a4932c4255aae7
David <david@hashlabs.com>
2016-04-07 10:58:54 -0430

    Add code to show typing on index and main.js
    Only is need add a <p> with de new text on <div id="typed-strings">
    to show new message.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-new WOW().init();
+new WOW().init(); // Trigger for animation
+
+// Typed.js function
+$(function(){
+  $("#typed").typed({
+    stringsElement: $('#typed-strings'),
+    typeSpeed: 40,
+    backDelay: 500,
+    loop: true,
+    contentType: 'html',
+    loopCount: 2,
+  });
+});
