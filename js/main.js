120c1ac0c7c4fd332a9077e0197e836fa10a881e
David <david@hashlabs.com>
2016-04-07 13:51:16 -0430

    Adapt mobile menu to our need, need polish style
    New approach to show header went scroll up, need polish style because
    menu in mobile are in early stage

--- js/main.js
+++ js/main.js
@@ -30,54 +30,54 @@ detachPoint = 650, // point of detach (after scroll passed it, menu is fixed)
 hideShowOffset = 6; // scrolling value after which triggers hide/show menu
 // on scroll hide/show menu
 $(window).scroll(function() {
-  if (!$('nav').hasClass('expanded')) {
+  if (!$('#site-nav').hasClass('expanded')) {
     var currentScroll = $(this).scrollTop(), // gets current scroll position
     scrollDifference = Math.abs(currentScroll - previousScroll); // calculates how fast user is scrolling
     // if scrolled past menu
     if (currentScroll > menuOffset) {
       // if scrolled past detach point add class to fix menu
       if (currentScroll > detachPoint) {
-        if (!$('nav').hasClass('detached'))
-        $('nav').addClass('detached');
+        if (!$('#site-nav').hasClass('detached'))
+        $('#site-nav').addClass('detached');
       }
       // if scrolling faster than hideShowOffset hide/show menu
       if (scrollDifference >= hideShowOffset) {
         if (currentScroll > previousScroll) {
           // scrolling down; hide menu
-          if (!$('nav').hasClass('invisible'))
-          $('nav').addClass('invisible');
+          if (!$('#site-nav').hasClass('invisible'))
+          $('#site-nav').addClass('invisible');
         } else {
           // scrolling up; show menu
-          if ($('nav').hasClass('invisible'))
-          $('nav').removeClass('invisible');
+          if ($('#site-nav').hasClass('invisible'))
+          $('#site-nav').removeClass('invisible');
         }
       }
     } else {
       // only remove “detached” class if user is at the top of document (menu jump fix)
       if (currentScroll <= 0){
-        $('nav').removeClass();
+        $('#site-nav').removeClass();
       }
     }
     // if user is at the bottom of document show menu
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-      $('nav').removeClass('invisible');
+      $('#site-nav').removeClass('invisible');
     }
     // replace previous scroll position with new one
     previousScroll = currentScroll;
   }
 });
 // shows/hides navigation’s popover if class "expanded"
-$('nav').on('click touchstart', function(event) {
+$('.mobile--toggle').on('click touchstart', function(event) {
   showHideNav();
   event.preventDefault();
 });
 // clicking anywhere inside navigation or heading won’t close navigation’s popover
-$('#navigation').on('click touchstart', function(event){
+$('#site-nav-mobile').on('click touchstart', function(event){
   event.stopPropagation();
 });
 // checks if navigation’s popover is shown
 function showHideNav() {
-  if ($('nav').hasClass('expanded')) {
+  if ($('#site-nav').hasClass('expanded')) {
     hideNav();
   } else {
     showNav();
@@ -85,23 +85,21 @@ function showHideNav() {
 }
 // shows the navigation’s popover
 function showNav() {
-  $('nav').removeClass('invisible').addClass('expanded');
-  $('#container').addClass('blurred');
+  $('#site-nav').removeClass('invisible').addClass('expanded');
   window.setTimeout(function(){$('body').addClass('no_scroll');}, 200); // Firefox hack. Hides scrollbar as soon as menu animation is done
-  $('#navigation a').attr('tabindex', ''); // links inside navigation should be TAB selectable
+  $('#site-nav-mobile a').attr('tabindex', ''); // links inside navigation should be TAB selectable
 }
 // hides the navigation’s popover
 function hideNav() {
-  $('#container').removeClass('blurred');
   window.setTimeout(function(){$('body').removeClass();}, 10); // allow animations to start before removing class (Firefox)
-  $('nav').removeClass('expanded');
-  $('#navigation a').attr('tabindex', '-1'); // links inside hidden navigation should not be TAB selectable
+  $('#site-nav').removeClass('expanded');
+  $('#site-nav-mobile a').attr('tabindex', '-1'); // links inside hidden navigation should not be TAB selectable
   $('.icon').blur(); // deselect icon when navigation is hidden
 }
 // keyboard shortcuts
 $('body').keydown(function(e) {
   // menu accessible via TAB as well
-  if ($("nav .icon").is(":focus")) {
+  if ($("#site-nav .icon").is(":focus")) {
     // if ENTER/SPACE show/hide menu
     if (e.keyCode === 13 || e.keyCode === 32) {
       showHideNav();
