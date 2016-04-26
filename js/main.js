4e6d6f4f2626bd103e53bbee60a0ec6edb06fe84
Carlos <carlos@hashlabs.com>
2016-04-26 11:26:53 -0430

    Add headroom.js to clean code in the main.js
    In this commit we added headroom.js to get rid of all the nested ifs in
    the main.js file. Headroom has the functionality to bind to events when
    the user scrolls down or up the page, also has events for when the page
    is at its top or at its bottom.
    
    We modified the styles from the mobile-menu.scss so the classes override
    correctly when were on top or bottom of the page.

--- js/main.js
+++ js/main.js
@@ -24,50 +24,21 @@ $(function(){
   });
 
 
-/****************** function for menu hide and show ***************************/
+/****************** function for menu hide and show **************************
+*/
+var menuOffset = 50;
 
-var previousScroll = 0, // previous scroll position
-menuOffset = 50, // height of menu (once scroll passed it, menu is hidden)
-detachPoint = 560, // point of detach (after scroll passed it, menu is fixed)
-hideShowOffset = 6; // scrolling value after which triggers hide/show menu
-// on scroll hide/show menu
-$(window).scroll(function() {
-  if (!$('#site-nav').hasClass('expanded')) {
-    var currentScroll = $(this).scrollTop(), // gets current scroll position
-    scrollDifference = Math.abs(currentScroll - previousScroll); // calculates how fast user is scrolling
-    // if scrolled past menu
-    if (currentScroll > menuOffset) {
-      // if scrolled past detach point add class to fix menu
-      if (currentScroll > detachPoint) {
-        if (!$('#site-nav').hasClass('detached'))
-        $('#site-nav').addClass('detached');
-      }
-      // if scrolling faster than hideShowOffset hide/show menu
-      if (scrollDifference >= hideShowOffset) {
-        if (currentScroll > previousScroll) {
-          // scrolling down; hide menu
-          if (!$('#site-nav').hasClass('invisible'))
-          $('#site-nav').addClass('invisible');
-        } else {
-          // scrolling up; show menu
-          if ($('#site-nav').hasClass('invisible'))
-          $('#site-nav').removeClass('invisible');
-        }
-      }
-    } else {
-      // only remove “detached” class if user is at the top of document (menu jump fix)
-      if (currentScroll <= 0){
-        $('#site-nav').removeClass();
-      }
+var myElement = document.querySelector("#site-nav");
+var headroom  = new Headroom(myElement, {
+    offset: menuOffset,
+    classes: {
+      pinned: "detached", // scrolling up
+      bottom: "fixed", // bottom of the page
+      top: "default" // top of the page
     }
-    // if user is at the bottom of document show menu
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-      $('#site-nav').removeClass('invisible');
-    }
-    // replace previous scroll position with new one
-    previousScroll = currentScroll;
-  }
 });
+headroom.init();
+
 // shows/hides navigation’s popover if class "expanded"
 $('.mobile-toggle').on('click touchstart', function(event) {
   showHideNav();
@@ -87,52 +58,13 @@ function showHideNav() {
 }
 // shows the navigation’s popover
 function showNav() {
-  $('#site-nav').removeClass('invisible').addClass('expanded');
+  $('#site-nav').addClass('expanded');
   $('.mobile-toggle').addClass('is-active'); // menu toggle animation to X
-  window.setTimeout(function(){$('body').addClass('no_scroll');}, 200); // Firefox hack. Hides scrollbar as soon as menu animation is done
+  $('body').addClass('no_scroll');
   }
 // hides the navigation’s popover
 function hideNav() {
-  window.setTimeout(function(){$('body').removeClass();}, 10); // allow animations to start before removing class (Firefox)
   $('#site-nav').removeClass('expanded');
   $('.mobile-toggle').removeClass('is-active'); // menu toggle animation to burger icon
+  $('body').removeClass();
 }
-
-// keyboard shortcuts
-$('body').keydown(function(e) {
-  // menu accessible via TAB as well
-  if ($("#site-nav .icon").is(":focus")) {
-    // if ENTER/SPACE show/hide menu
-    if (e.keyCode === 13 || e.keyCode === 32) {
-      showHideNav();
-      e.preventDefault();
-    }
-  }
-  // if ESC show/hide menu
-  if (e.keyCode === 27 || e.keyCode === 77) {
-    showHideNav();
-    e.preventDefault();
-  }
-
-});
-
-/* function to show animation in menu button*/
-(function() {
-
-  "use strict";
-
-  var toggles = document.querySelectorAll('.mobile-toggle');
-
-  for (var i = toggles.length - 1; i >= 0; i--) {
-    var toggle = toggles[i];
-    toggleHandler(toggle);
-  }
-
-  function toggleHandler(toggle) {
-    toggle.addEventListener('click', function(e) {
-      e.preventDefault();
-      (this.classList.contains('is-active') === true) ? this.classList.remove('is-active') : this.classList.add('is-active');
-    });
-  }
-
-})();
