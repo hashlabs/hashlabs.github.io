11815a2074db2710898d1472564f296c1845a319
David <david@hashlabs.com>
2016-04-26 09:09:23 -0430

    Change config of type.js, and delete file no need
    Change main.js to delete cursor and add more time of read, also
    delete file of cursor animation and update main.scss

--- js/main.js
+++ js/main.js
@@ -3,11 +3,23 @@ new WOW().init(); // Trigger for animation
 // Typed.js function
 $(function(){
   $("#typed").typed({
+    // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
     stringsElement: $('#typed-strings'),
-    typeSpeed: 40,
-    backDelay: 500,
-    loop: true,
+    // typing speed
+    typeSpeed: 30,
+    // time before typing starts
+    startDelay: 80,
+    // backspacing speed
+    backSpeed: 5,
+    // time before backspacing
+    backDelay: 1800,
+    // loop
+    loop: false,
+    // false = infinite
+    loopCount: false,
+    // show cursor
+    showCursor: false,
+    // either html or text
     contentType: 'html',
-    loopCount: 2,
   });
 });
