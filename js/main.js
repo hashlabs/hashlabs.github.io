d0fd2f74f6277d3de3c0a96bf6e27d93f5060138
Carlos <carlos@hashlabs.com>
2016-04-28 13:22:10 -0430

    Replace Headroom with TeehanLax
    In this commit we replaced the Headroom script for the navbar
    functionality with TeehanLax, a script that better resembles what we
    need in Labcoat

--- js/main.js
+++ js/main.js
@@ -13,48 +13,59 @@ $(document).ready(function () {
     contentType: 'html',
   });
 
-  var previousScroll = 0, // previous scroll position
-  menuOffset = 100, // height of menu (once scroll passed it, menu is hidden)
-  detachPoint = 560, // point of detach (after scroll passed it, menu is fixed)
-  hideShowOffset = 6; // scrolling value after which triggers hide/show menu
-  // on scroll hide/show menu
-  $(window).scroll(function() {
-    if (!$('#site-nav').hasClass('expanded')) {
-      var currentScroll = $(this).scrollTop(), // gets current scroll position
-      scrollDifference = Math.abs(currentScroll - previousScroll); // calculates how fast user is scrolling
-      // if scrolled past menu
-      if (currentScroll > menuOffset) {
-        // if scrolled past detach point add class to fix menu
-        if (currentScroll > detachPoint) {
-          if (!$('#site-nav').hasClass('header-detached'))
-          $('#site-nav').addClass('header-detached');
-        }
-        // if scrolling faster than hideShowOffset hide/show menu
-        if (scrollDifference >= hideShowOffset) {
-          if (currentScroll > previousScroll) {
-            // scrolling down; hide menu
-            if (!$('#site-nav').hasClass('header-hidden'))
-            $('#site-nav').addClass('header-hidden');
-          } else {
-            // scrolling up; show menu
-            if ($('#site-nav').hasClass('header-hidden'))
-            $('#site-nav').removeClass('header-hidden');
-          }
-        }
-      } else {
-        // only remove “header-detached” class if user is at the top of document (menu jump fix)
-        if (currentScroll <= 0){
-          $('#site-nav').removeClass("header-hidden header-detached");
-        }
-      }
-      // if user is at the bottom of document show menu
-      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        $('#site-nav').removeClass('header-hidden');
-      }
-      // replace previous scroll position with new one
-      previousScroll = currentScroll;
+  var navbarHeader = $("#site-nav");
+  var teehanNav  = new TeehanLax(navbarHeader, {
+    classes: {
+      detached: "header-detached",
+      hidden: "header-hidden",
+      expanded: "expanded"
     }
   });
+
+  teehanNav.init();
+
+  // var previousScroll = 0,
+  // menuOffset = 100,
+  // detachPoint = 560,
+  // hideShowOffset = 6;
+
+  // $(window).scroll(function() {
+  //   if (!$('#site-nav').hasClass('expanded')) {
+  //     var currentScroll = $(this).scrollTop(),
+  //     scrollDifference = Math.abs(currentScroll - previousScroll); //
+
+  //     if (currentScroll > menuOffset) {
+
+  //       if (currentScroll > detachPoint) {
+  //         if (!$('#site-nav').hasClass('header-detached'))
+  //         $('#site-nav').addClass('header-detached');
+  //       }
+
+  //       if (scrollDifference >= hideShowOffset) {
+  //         if (currentScroll > previousScroll) {
+
+  //           if (!$('#site-nav').hasClass('header-hidden'))
+  //           $('#site-nav').addClass('header-hidden');
+  //         } else {
+
+  //           if ($('#site-nav').hasClass('header-hidden'))
+  //           $('#site-nav').removeClass('header-hidden');
+  //         }
+  //       }
+  //     } else {
+
+  //       if (currentScroll <= 0){
+  //         $('#site-nav').removeClass("header-hidden header-detached");
+  //       }
+  //     }
+
+  //     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+  //       $('#site-nav').removeClass('header-hidden');
+  //     }
+
+  //     previousScroll = currentScroll;
+  //   }
+  // });
 
   $('.mobile-toggle').on('click touchstart', function(event) {
     showHideNav();
---
+++ js/teehanNavigation.js
@@ -0,0 +1,215 @@
+(function (root, factory) {
+  root.TeehanLax = factory();
+}(this, function() {
+  'use strict';
+
+  /**
+   * Handles debouncing of events via requestAnimationFrame
+   * @see http://www.html5rocks.com/en/tutorials/speed/animations/
+   * @param {Function} callback The callback to handle whichever event
+   */
+  function Debouncer (callback) {
+    this.callback = callback;
+    this.ticking = false;
+  }
+
+  Debouncer.prototype = {
+    constructor : Debouncer,
+
+    /**
+     * dispatches the event to the supplied callback
+     * @private
+     */
+    update : function() {
+      this.callback && this.callback();
+      this.ticking = false;
+    },
+
+    /**
+     * ensures events don't get stacked
+     * @private
+     */
+    requestTick : function() {
+      if(!this.ticking) {
+        requestAnimationFrame(this.rafCallback || (this.rafCallback = this.update.bind(this)));
+        this.ticking = true;
+      }
+    },
+
+    /**
+     * Attach this as the event listeners
+     */
+    handleEvent : function() {
+      this.requestTick();
+    }
+  };
+
+  /**
+   * Check if object is part of the DOM
+   * @constructor
+   * @param {Object} obj element to check
+   */
+  function isDOMElement(obj) {
+    return obj && typeof window !== 'undefined' && (obj === window || obj.nodeType);
+  }
+
+  /**
+   * Helper function for extending objects
+   */
+  function extend (object /*, objectN ... */) {
+    if(arguments.length <= 0) {
+      throw new Error('Missing arguments in extend function');
+    }
+
+    var result = object || {},
+        key,
+        i;
+
+    for (i = 1; i < arguments.length; i++) {
+      var replacement = arguments[i] || {};
+
+      for (key in replacement) {
+        // Recurse into object except if the object is a DOM element
+        if(typeof result[key] === 'object' && ! isDOMElement(result[key])) {
+          result[key] = extend(result[key], replacement[key]);
+        }
+        else {
+          result[key] = result[key] || replacement[key];
+        }
+      }
+    }
+
+    return result;
+  }
+
+  function TeehanLax(jqueryElement, options) {
+    options = extend(options, TeehanLax.options);
+
+    this.elem = jqueryElement;
+    this.classes = options.classes;
+    this.scroller = window;
+    this.initialised = false;
+    this.lastKnownScrollY = 0;
+    this.menuOffset = 100;
+    this.detachPoint = 500;
+    this.hideShowOffset = 6;
+  }
+
+  TeehanLax.prototype = {
+    constructor: TeehanLax,
+
+    init: function () {
+      this.debouncer = new Debouncer(this.update.bind(this));
+
+      // defer event registration to handle browser
+      // potentially restoring previous scroll position
+      setTimeout(this.attachEvent.bind(this), 100);
+
+      return this;
+    },
+
+    attachEvent: function() {
+      if(!this.initialised){
+        this.initialised = true;
+        this.lastKnownScrollY = this.getScrollY();
+
+        this.scroller.addEventListener('scroll', this.debouncer, false);
+
+        this.debouncer.handleEvent();
+      }
+    },
+
+    /**
+     * Gets the Y scroll position
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/Window.scrollY
+     * @return {Number} pixels the page has scrolled along the Y-axis
+     */
+    getScrollY: function() {
+      return (this.scroller.pageYOffset !== undefined)
+        ? this.scroller.pageYOffset
+        : (this.scroller.scrollTop !== undefined)
+          ? this.scroller.scrollTop
+          : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    },
+
+    detach: function() {
+      var classes = this.classes;
+
+      if(!this.elem.hasClass(classes.detached)) {
+        this.elem.addClass(classes.detached);
+      }
+    },
+
+    top: function() {
+      var classes = this.classes;
+
+      this.elem.removeClass(classes.detached);
+      this.elem.removeClass(classes.hidden);
+    },
+
+    bottom: function() {
+      var classes = this.classes;
+
+      if(this.elem.hasClass(classes.hidden)) {
+        this.elem.removeClass(classes.hidden);
+      }
+    },
+
+    showNav: function(movingUp) {
+      var classes = this.classes;
+
+      if(movingUp) {
+        if(!this.elem.hasClass(classes.hidden)) {
+          this.elem.addClass(classes.hidden);
+        }
+      } else {
+        if(this.elem.hasClass(classes.hidden)) {
+          this.elem.removeClass(classes.hidden);
+        }
+      }
+    },
+
+    shouldUpdateState: function(scrollDifference) {
+      return scrollDifference >= this.hideShowOffset;
+    },
+
+    update: function() {
+      var currentScrollY = this.getScrollY(),
+      previousScrollY = this.lastKnownScrollY,
+      scrollDifference = Math.abs(currentScrollY - previousScrollY);
+
+      if(currentScrollY > this.menuOffset) {
+        if(currentScrollY > this.detachPoint) {
+          this.detach();
+        }
+
+        if(this.shouldUpdateState(scrollDifference)) {
+          this.showNav(currentScrollY > previousScrollY);
+        }
+      } else {
+        if(currentScrollY <= 0) {
+          this.top();
+        }
+      }
+
+      if((currentScrollY + window.innerHeight) >= document.body.offsetHeight) {
+        this.bottom();
+      }
+
+      this.lastKnownScrollY = currentScrollY;
+    }
+  };
+
+  TeehanLax.options = {
+    menuOffset: 100,
+    detachPoint: 600,
+    hideShowOffset: 6,
+    classes: {
+      detached: "nav-detached",
+      hidden: "nav-hidden",
+      expanded: "nav-expanded"
+    }
+  }
+
+  return TeehanLax;
+}));
