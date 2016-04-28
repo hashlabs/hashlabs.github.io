ca885f6323e06296b4722b3c65a716b16abd7d93
Carlos <carlos@hashlabs.com>
2016-04-28 10:44:37 -0430

    Replaced headroom.js with a custom js for the nav
    In this commit I replaced headroom because It wasn't working as expected
    in every browser, the code was starting to go crazy, so we're going to
    take another approach.

--- js/main.js
+++ js/main.js
@@ -13,21 +13,48 @@ $(document).ready(function () {
     contentType: 'html',
   });
 
-  var menuOffset = 0;
-  var myElement = document.querySelector("#site-nav");
-  var headroom  = new Headroom(myElement, {
-    offset: menuOffset,
-    classes: {
-      pinned: "slideInDown",
-      unpinned: "slideOutUp",
-      bottom: "site-nav-bottom",
-      top: "site-nav-top",
-      notTop: "site-nav-not-top"
-    },
-    tolerance: 3
+  var previousScroll = 0, // previous scroll position
+  menuOffset = 100, // height of menu (once scroll passed it, menu is hidden)
+  detachPoint = 560, // point of detach (after scroll passed it, menu is fixed)
+  hideShowOffset = 6; // scrolling value after which triggers hide/show menu
+  // on scroll hide/show menu
+  $(window).scroll(function() {
+    if (!$('#site-nav').hasClass('expanded')) {
+      var currentScroll = $(this).scrollTop(), // gets current scroll position
+      scrollDifference = Math.abs(currentScroll - previousScroll); // calculates how fast user is scrolling
+      // if scrolled past menu
+      if (currentScroll > menuOffset) {
+        // if scrolled past detach point add class to fix menu
+        if (currentScroll > detachPoint) {
+          if (!$('#site-nav').hasClass('header-detached'))
+          $('#site-nav').addClass('header-detached');
+        }
+        // if scrolling faster than hideShowOffset hide/show menu
+        if (scrollDifference >= hideShowOffset) {
+          if (currentScroll > previousScroll) {
+            // scrolling down; hide menu
+            if (!$('#site-nav').hasClass('header-hidden'))
+            $('#site-nav').addClass('header-hidden');
+          } else {
+            // scrolling up; show menu
+            if ($('#site-nav').hasClass('header-hidden'))
+            $('#site-nav').removeClass('header-hidden');
+          }
+        }
+      } else {
+        // only remove “header-detached” class if user is at the top of document (menu jump fix)
+        if (currentScroll <= 0){
+          $('#site-nav').removeClass("header-hidden header-detached");
+        }
+      }
+      // if user is at the bottom of document show menu
+      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        $('#site-nav').removeClass('header-hidden');
+      }
+      // replace previous scroll position with new one
+      previousScroll = currentScroll;
+    }
   });
-
-  headroom.init();
 
   $('.mobile-toggle').on('click touchstart', function(event) {
     showHideNav();
@@ -55,7 +82,7 @@ $(document).ready(function () {
   function hideNav() {
     $('#site-nav').removeClass('expanded');
     $('.mobile-toggle').removeClass('is-active');
-    $('body').removeClass();
+    $('body').removeClass('no_scroll');
   }
 });
 
