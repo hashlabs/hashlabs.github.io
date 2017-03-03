936dd9af2decfb9c480924dc1c70625b1212b129
Victor Tortolero <victormtortolero@gmail.com>
2017-03-03 16:24:29 -0400

    Homepage had to be remade using new design, so this commit issues that by:
    - Added lead section.
    - Added brands section.
    - Added what we do section.
    - Added techonologies/tools section.
    - Added how section.
    - Added CAT (call to action) section.
    - Added FAQ section.
    
    All these sections have their own styles, and were done as separate
    components, so they are easy to modify, remove or reorder. Some listing
    sections were made with flexbox, so some polyfill should be added for it,
    already created issue for that.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
+var videoElement;
 $(document).ready(function () {
   var mobileMenu = $('#mobile-nav');
   var siteNav = $('.site-navbar');
   var mobileMenuToggle = $('#mobile-nav-toggle');
+  var video = $('#lead-video');
+  videoElement = video[0];
+
   $('.site-navbar').headroom();
+  $('p').widont();
+  $('h2').widont();
+  $('h3').widont();
+
+  // jQuery(function($) {
+  //   $('h1,h2,h3,li,p').each(function() {
+  //     $(this).html($(this).html().replace(/\s([^\s<]{0,10})\s*$/,'&nbsp;$1'));
+  //   });
+  // });
 
   mobileMenuToggle.on('click', function(event) {
     mobileMenu.toggleClass('active');
     siteNav.toggleClass('active');
+  });
+
+  video.on('ended', function(event) {
+    videoElement.currentTime = 10.064;
+    videoElement.play();
   });
 });
